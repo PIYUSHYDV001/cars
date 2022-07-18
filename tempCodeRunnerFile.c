3362ab7f361e3e9ee5fa26db11f53b647197cc7a
PIYUSH YADAV <106904666+PIYUSHYDV001@users.noreply.github.com>
2022-07-18 09:32:41 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+int main ()
+{
+do  
+{  
+   printf("All the way up"); 
+   /* initializing main body of the infinite do while loop.. */  
+}
+while (1); /* 1 is considered as true and 0 as false */
+}
+
